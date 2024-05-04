46732731d3ea216374ea538ca6e246877e2df7c9
HashWarlock <hashwarlock@protonmail.com>
2024-05-03 21:34:17 -0500

    Update code and README to use agents.phala.network for gateway. Still problem with render HTML

---
+++ src/fewShotPromptExamples.ts
@@ -0,0 +1,63 @@
+export const fewShotPromptExamples = [
+    {
+        "tweet": `Introduce 🤖 Agent Contract 🤖
+            🔨 Create and host AI Agent with
+            @PhalaNetwork
+             like smart contracts, to build a Multi-Agents World.
+            👋 Vision shared together with
+            @david_enim
+
+            @BrianknowsAI
+
+            @AlgoveraAI
+
+            @autonolas
+
+            🧰 Support
+            @Auto_GPT
+
+            @LangChainAI
+
+
+            💡Idea and design made in #ETHDenver2024
+            https://youtu.be/TEAFVKEV2oc`,
+    },
+    {
+        "tweet": `Wow this is wide!
+            A
+            @RiscZero
+             host runtime is added to
+            @PhalaNetwork
+              js runtime 🫡
+            A milestone for TEE+ZKP multi-prover strategy🔥`,
+    },
+    {
+        "tweet": `🚀🔥
+            @binance
+              is throwing down the gauntlet, and it's time for
+            @PhalaNetwork
+              to make some noise!
+
+            YOUR VOTE decides if  they will list $PHA on the futures market! 💥
+
+            Hit up this link, make your mark, and let's blast $PHA (https://binance.com/en/futures/next) 🌌🔥`,
+    },
+    {
+        "tweet": `🐞Find runtime bugs for
+            @PhalaNetwork
+             ?
+            💰Get bounty from $60,500 prize pool !
+            https://code4rena.com/audits/2024-03-phat-contract-runtime`,
+    },
+    {
+        "tweet": `Taking flights to Denver is a crutch for weak people who don’t want to 🚗 ride for 36 hours—-
+            because taking flights have risks, and you have to trust the captain 🥱`,
+    },
+    {
+        "tweet": `Highlights from #ETHDenver2024 🚀:
+            - AI transcends the hype, proving its concrete value in the ecosystem.
+            - Unveiling real-world, practical approaches to decentralizing AI services.
+            - Beyond ZKML, numerous projects are innovating with AI Agent solutions. 🛠️
+            - While "AI as a Player" dominates current Web3 applications, expect a wave of groundbreaking innovations on the horizon. 💡`,
+    }
+];
